535c07741d49c03fcf6122cba95a81355367acd5
bek07kg <bekamurzaev4@gmail.com>
2023-08-31 00:45:12 +0300

    getx

---
+++ lib/src/app/app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:news_get_x/src/views/home_view.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'News GetX',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomeView(),
+    );
+  }
+}
---
+++ lib/src/constants/api_const.dart
@@ -0,0 +1,10 @@
+class ApiConst {
+  static const apiKey = "5b245bdd269b4324a88ce005cbfcd32e";
+  static String topNews([String? domain]) =>
+      'https://newsapi.org/v2/top-headlines?country=${domain ?? 'us'}&category=business&apiKey=$apiKey';
+  static String searchNew(String text) =>
+      'https://newsapi.org/v2/everything?q=$text&from=2023-07-30&sortBy=publishedAt&apiKey=$apiKey';
+
+  static const String image =
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpBAZafcETIewM5tuVRKUWoqpVNB5CKrIbGzOIB8IxWx40WQ2DK2MDkmbEin9_mu1K54Y&usqp=CAU';
+}
